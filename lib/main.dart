14d34bbd444a8a915d097c7f1463ed2bcb686176
Omar Jabri <omarjabri967@gmail.com>
2023-01-01 16:09:15 +0000

    added ramosa icon in app

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,11 @@ import 'package:ramosa_apps/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // WidgetsFlutterBinding.ensureInitialized();
   runApp(const Ramosa());
 }
 
